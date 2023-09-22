a04b13a85b582e3353526d5e61c449966ae74393
Danny Yum <dhyum9@gmail.com>
2023-09-22 17:20:48 -0600

    Create MenuItem model, and to_dict method within model

--- app/models/menu_item.py
+++ app/models/menu_item.py
@@ -1,4 +1,5 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
 
 
 class MenuItem(db.Model):
@@ -8,15 +9,24 @@ class MenuItem(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    ownerId = db.Column(db.Integer, nullable=False, unique=True)
-    email = db.Column(db.String(255), nullable=False, unique=True)
-    hashed_password = db.Column(db.String(255), nullable=False)
-
-
+    restaurantId = db.Column(db.Integer, db.ForeignKey("restaurants.id"), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    calories = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    imageUrl = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime)
+    updated_at = db.Column(db.DateTime, default=datetime)
 
     def to_dict(self):
         return {
             'id': self.id,
-            'username': self.username,
-            'email': self.email
+            'restaurantId': self.restaurantId,
+            'name': self.name,
+            'calories': self.calories,
+            'price': self.price,
+            'description': self.description,
+            'imageUrl': self.imageUrl,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
         }
