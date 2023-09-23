19998f45b8dfa7514b4eb9105601e589debfd945
ehab <ehab41976@gmail.com>
2023-09-23 01:01:40 -0700

    review seeds

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,6 @@
 from .db import db
 from .user import User
 from .db import environment, SCHEMA
+from .review import Review
+from .restaurant import Restaurant
+
--- app/models/review.py
+++ app/models/review.py
@@ -1,17 +1,17 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
-from werkzeug.security import generate_password_hash, check_password_hash
+
 from flask_login import UserMixin
 from datetime import datetime
 
-class Review(db.Model):
+class Review(db.Model, UserMixin):
     __tablename__ = 'reviews'
 
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    restaurantId = db.Column(db.Integer, db.ForeignKey("restaurants.id"), nullable=False)
-    userId = db.Column(db.Integer,db.ForeignKey("users.id"), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"), nullable=False)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"), nullable=False)
     review = db.Column(db.String(255),nullable=False)
     stars = db.Column(db.Integer)
     created_at = db.Column(db.DateTime, default=datetime)
@@ -19,6 +19,18 @@ class Review(db.Model):
 
 #relationships
 
-    restaurant = db.relationship("Restaurant", back_populates = "review_restaurant")
+    restaurant = db.relationship("Restaurant", back_populates = "review")
 
-    user = db.relationship("User", back_populates = "review_user")
+    user = db.relationship("User", back_populates = "user_review")
+
+# convert to dictionary
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'restaurant_id': self.restaurant_id,
+            'user_id': self.user_id,
+            'review': self.review,
+            'stars': self.stars
+
+    }
