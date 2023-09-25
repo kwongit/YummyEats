f4c32b67c5fef31b25981ffee1801d454e2a76fc
ehab <ehab41976@gmail.com>
2023-09-25 01:53:37 -0700

    some css for restaurants

--- react-app/src/components/Restaurants/index.js
+++ react-app/src/components/Restaurants/index.js
@@ -1,5 +1,11 @@
 import { useState, useEffect } from "react";
 import RestaurantTile from "../RestaurantTile";
+import './all-restaurants.css'
+import categories from "../Restaurants/categories"
+import bogo from "../../assets/bogo.png"
+import overall from "../../assets/top_eats.png"
+
+
 
 export const Restaurants = () => {
   const [restaurants, setRestaurants] = useState([]);
@@ -12,12 +18,19 @@ export const Restaurants = () => {
   }, []);
 
   return (
-    <div className="restaurant-details-container">
-      <h1>RESTAURANTS TEST PAGE</h1>
-      {restaurants.map((restaurant) => (
-        <RestaurantTile key={restaurant.id} restaurant={restaurant} />
-      ))}
-    </div>
+    <>
+      {categories}
+      <hr id="cat-hr"></hr>
+      <div id="middle-header-container">
+        <h1>RESTAURANTS TEST PAGE</h1>
+        <h1>Crave it? Get it.</h1>
+        <img src={bogo}></img>
+      </div>
+      <div className="restaurant-details-container">
+        {restaurants.map((restaurant) => (
+          <RestaurantTile key={restaurant.id} restaurant={restaurant} />
+        ))}
+      </div>
+    </>
   );
 };
-
