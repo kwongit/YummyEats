cbb56883455b52a93846b0f90259acf1bb33b9a0
Danny Yum <dhyum9@gmail.com>
2023-09-26 16:13:41 -0600

    Relocate menu item routes from menu_item_routes.py to restaurant_routes.py

--- app/api/menu_item_routes.py
+++ app/api/menu_item_routes.py
@@ -8,10 +8,6 @@ from flask_login import current_user, login_required
 menu_item_routes = Blueprint('menu_item', __name__)
 
 
-# @menu_item_routes.route('/menu_items', methods=['GET'])
-# def get_menu_items_by_id(restaurantId):
-#     """Query to get all menu items from a specified restaurants"""
-
 @menu_item_routes.route('/<int:id>')
 #/api/menuitems/menuItemId
 def get_menu_item_by_id(id):
@@ -26,51 +22,6 @@ def get_menu_item_by_id(id):
 
     return one_menu_item.to_dict()
 
-@menu_item_routes.route('/<int:restaurantId>/menuitems')
-#/api/restaurants/:restaurantId/menuitems
-def get_restaurant_menu_items(restaurantId):
-    """
-    Query for all menu items for a specific restaurant
-    """
-
-    all_menu_items = MenuItem.query.all()
-
-    restaurant_menu_items = [menu_item.to_dict() for menu_item in all_menu_items if menu_item.restaurantId == restaurantId]
-
-    return restaurant_menu_items
-
-@menu_item_routes.route('/<int:restaurantId>/createmenuitem', methods=["POST"])
-#/api/restaurants/:restaurantId/createmenuitem
-@login_required
-def create_menu_item(restaurantId):
-    """
-    Route to post a new menu item
-    """
-
-    form = MenuItemForm()
-
-    form["csrf_token"].data = request.cookies["csrf_token"]
-
-    if form.validate_on_submit():
-
-        new_menu_item = MenuItem(
-            restaurantId=restaurantId,
-            name=form.data["name"],
-            size=form.data["size"],
-            calories=form.data["calories"],
-            price=form.data["price"],
-            description=form.data["description"],
-            imageUrl=form.data["image_url"],
-            created_at = date.today(),
-            updated_at = date.today()
-        )
-        db.session.add(new_menu_item)
-        db.session.commit()
-        return new_menu_item.to_dict(), 201
-
-    else:
-        print(form.errors)
-        return { "errors": form.errors }, 400
 
 @menu_item_routes.route("/<int:menuItemId>", methods=["DELETE"])
 @login_required
--- app/api/restaurant_routes.py
+++ app/api/restaurant_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request, redirect
-from app.models import Restaurant, User, Review
+from app.models import Restaurant, User, Review, MenuItem
 from ..forms.restaurant_form import RestaurantForm
+from ..forms.menu_item_form import MenuItemForm
 from datetime import date
 from ..models.db import db
 from flask_login import current_user, login_required
@@ -172,3 +173,49 @@ def delete(restaurantId):
             return { "message": "FORBIDDEN" }, 403
     else:
         return { "message": "Restaurant not found!" }, 404
+
+@restaurant_routes.route('/<int:restaurantId>/menuitems')
+#/api/restaurants/:restaurantId/menuitems
+def get_restaurant_menu_items(restaurantId):
+    """
+    Query for all menu items for a specific restaurant
+    """
+
+    all_menu_items = MenuItem.query.all()
+
+    restaurant_menu_items = [menu_item.to_dict() for menu_item in all_menu_items if menu_item.restaurantId == restaurantId]
+
+    return restaurant_menu_items
+
+@restaurant_routes.route('/<int:restaurantId>/createmenuitem', methods=["POST"])
+#/api/restaurants/:restaurantId/createmenuitem
+@login_required
+def create_menu_item(restaurantId):
+    """
+    Route to post a new menu item
+    """
+
+    form = MenuItemForm()
+
+    form["csrf_token"].data = request.cookies["csrf_token"]
+
+    if form.validate_on_submit():
+
+        new_menu_item = MenuItem(
+            restaurantId=restaurantId,
+            name=form.data["name"],
+            size=form.data["size"],
+            calories=form.data["calories"],
+            price=form.data["price"],
+            description=form.data["description"],
+            imageUrl=form.data["image_url"],
+            created_at = date.today(),
+            updated_at = date.today()
+        )
+        db.session.add(new_menu_item)
+        db.session.commit()
+        return new_menu_item.to_dict(), 201
+
+    else:
+        print(form.errors)
+        return { "errors": form.errors }, 400
