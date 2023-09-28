537d5298c370de1545e5a16b3cea8404786fd7f5
Danny Yum <dhyum9@gmail.com>
2023-09-28 08:56:03 -0600

    Change Delete button so that it conditionally renders

--- react-app/src/components/MenuItemTile/index.js
+++ react-app/src/components/MenuItemTile/index.js
@@ -1,17 +1,23 @@
 import { useHistory } from "react-router";
 import OpenModalButton from "../OpenModalButton";
 import { DeleteMenuItemModal } from "./DeleteMenuItemModal";
+import { useSelector } from "react-redux";
 
 
-const MenuItemTile = ({ menuItem }) => {
+const MenuItemTile = ({ menuItem, restaurantId }) => {
   const { id, name, price, imageUrl } = menuItem;
-// console.log('image url ====>' , image_url)
   const history = useHistory();
 
   const handleClick = () => {
     // tbd
     history.push(`/menuitems/${id}`);
   };
+
+  const restaurant = useSelector(
+    (state) => state.restaurant.singleRestaurant
+  );
+
+  const currentUser = useSelector(state => state.session.user);
 
   return (
     <div className="menu-item-details-container" key={id}>
@@ -26,13 +32,15 @@ const MenuItemTile = ({ menuItem }) => {
       <div>{price}</div>
       </div>
 
-      <OpenModalButton
-        className="delete-button"
-        buttonText="Delete"
-        modalComponent={
-          <DeleteMenuItemModal menuItemId={menuItem.id} />
-        }
-      />
+      {restaurant.owner_id === currentUser.id && (
+        <OpenModalButton
+          className="delete-button"
+          buttonText="Delete"
+          modalComponent={
+            <DeleteMenuItemModal menuItemId={menuItem.id} />
+          }
+        />
+      )}
     </div>
   );
 };
