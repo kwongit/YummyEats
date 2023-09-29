9bb77c8420d17b1270d2a6d3ff443cc21be5764a
Danny Yum <dhyum9@gmail.com>
2023-09-29 09:27:06 -0600

    Most recent commit

--- react-app/src/components/MenuItemTile/index.js
+++ react-app/src/components/MenuItemTile/index.js
@@ -18,7 +18,6 @@ const MenuItemTile = ({ menuItem, restaurantId }) => {
   const currentUser = useSelector((state) => state.session.user);
 
   return (
-<<<<<<< HEAD
     <div style={{display:"flex", flexDirection:"column"}}>
       <div className="menu-item-tile-container" key={id} onClick={handleClick}>
         <img
@@ -45,37 +44,6 @@ const MenuItemTile = ({ menuItem, restaurantId }) => {
           />
         )}
       </div>
-=======
-    <div className="menu-item-tile-container" key={id} onClick={handleClick}>
-      <img
-        className="menu-item-tile-image"
-        src={imageUrl}
-        alt={name}
-        title={name}
-      ></img>
-      <div className="menu-item-tile-info">
-        <div className="menu-item-tile-info-left-col">
-          <div className="menu-item-small-name">{name}</div>
-          <div className="menu-item-small-info">
-            ${price}
-            {calories && (
-              <>
-                <span style={{ fontWeight: "bold" }}> &#183;</span>{" "}
-                <span style={{ color: "grey" }}>{calories} Cal.</span>
-              </>
-            )}
-          </div>
-        </div>
-        <div className="menu-item-tile-info-right-col">
-          {currentUser && restaurant.owner_id === currentUser.id && (
-            <OpenModalButton
-              buttonText="Delete"
-              modalComponent={<DeleteMenuItemModal menuItemId={menuItem.id} />}
-            />
-          )}
-        </div>
-      </div>
->>>>>>> 4ca04bf728108c71365caddc86464352d2c886b2
     </div>
   );
 };
