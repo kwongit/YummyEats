b146b2c943fa6740894c3ac1b3c20ef438fc9f95
Danny Yum <dhyum9@gmail.com>
2023-10-17 10:53:09 -0600

    finish building search bar for restaurants page

--- react-app/src/components/SearchBar/index.js
+++ react-app/src/components/SearchBar/index.js
@@ -5,36 +5,49 @@ import { useHistory } from 'react-router-dom';
 const SearchBar = ({ placeholder, data}) => {
 const history = useHistory();
 const [filteredData, setFilteredData] = useState([]);
+const [wordEntered, setWordEntered] = useState("");
 
 const handleClick = (id) => {
   history.push(`/restaurants/${id}`);
 };
 
-const handleFilter = (e) => {
+const handleResults = (e) => {
   const searchWord = e.target.value.toLowerCase();
+  setWordEntered(searchWord);
   const newData = data.filter((value) => {
     return value.name.toLowerCase().includes(searchWord);
   })
-  setFilteredData(newData);
+  if(searchWord === ""){
+    setFilteredData([]);
+  } else {
+    setFilteredData(newData);
+  }
+}
+
+const clearInput = () => {
+  setFilteredData([]);
+  setWordEntered("");
 }
 
 return (
   <div className='search'>
-    <div className='searchInputs'>
+    <div className='search-bar-container'>
       <input
         type='text'
+        value={wordEntered}
+        className='search-bar'
         placeholder={placeholder}
-        onChange={handleFilter}
+        onChange={handleResults}
         >
       </input>
-      <div className='searchIcon'>
-        <i class="fa-solid fa-magnifying-glass"></i>
+      <div className='search-bar-icon'>
+        {wordEntered.length === 0 ? <i class="fa-solid fa-magnifying-glass"></i> : <i onClick={clearInput} id='search-clear-button' class="fa-solid fa-x"></i>}
       </div>
     </div>
     {filteredData.length !== 0 &&
-      <div className='dataResult'>
+      <div className='search-bar-results'>
         {filteredData.map((restaurant, key) => {
-          return <div className='dataItem' onClick={() => handleClick(restaurant.id)}>
+          return <div className='search-bar-result' onClick={() => handleClick(restaurant.id)}>
             <div>{restaurant.name}</div>
           </div>
         })}
