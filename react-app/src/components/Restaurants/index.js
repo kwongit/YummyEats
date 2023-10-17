553fd8be99fe0a847c0ba0a82a1c3967cf07a8b0
kevin <kevinwon30@gmail.com>
2023-10-17 15:42:33 -0700

    wip: remove alerts

--- react-app/src/components/Restaurants/index.js
+++ react-app/src/components/Restaurants/index.js
@@ -33,14 +33,26 @@ export const Restaurants = () => {
       {categories}
       <hr id="cat-hr"></hr>
       <div id="middle-header-container">
-        <div style={{display: "flex", flexDirection:"column", width: "250px"}}>
-          <h1 style={{textAlign:'left', margin: "40px 0 0 0"}}>Crave it? Get it.</h1>
-          <p style={{fontSize:"10px", color: "rgb(100,100,100)", marginLeft: "3px"}}>Search for a favorite restaurant, cuisine, or dish.</p>
+        <div
+          style={{ display: "flex", flexDirection: "column", width: "250px" }}
+        >
+          <h1 style={{ textAlign: "left", margin: "40px 0 0 0" }}>
+            Crave it? Get it.
+          </h1>
+          <p
+            style={{
+              fontSize: "10px",
+              color: "rgb(100,100,100)",
+              marginLeft: "3px",
+            }}
+          >
+            Search for a favorite restaurant, cuisine, or dish.
+          </p>
         </div>
         <div id="offers-container">
-        <img src={offer}onClick={showAlert}></img>
-        <img src={offer2} onClick={showAlert}></img>
-        <img src={offer3} onClick={showAlert}></img>
+          <img src={offer}></img>
+          <img src={offer2}></img>
+          <img src={offer3}></img>
         </div>
       </div>
       <div id="main-body-container">
