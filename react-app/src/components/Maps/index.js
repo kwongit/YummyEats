d63b59cab61dcc77a1a607908f60d53392b87a63
Kenny Doan <kdoan93@gmail.com>
2023-10-17 16:15:23 -0400

    WIP maps

--- react-app/src/components/Maps/index.js
+++ react-app/src/components/Maps/index.js
@@ -1,14 +1,21 @@
-import { useJsApiLoader, GoogleMap, Marker } from '@react-google-maps/api'
+import { useJsApiLoader, GoogleMap, Marker, useLoadScript } from '@react-google-maps/api'
 import "./Maps.css"
+
 
 const center = { lat: 37.8029, lng: -122.4484 }
 
 
 export const Map = () => {
 
-    const { isLoaded } = useJsApiLoader({
+    // const { isLoaded } = useJsApiLoader({
+    //     // Loads Google Maps script into component
+    //     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+    // })
+
+    const { isLoaded } = useLoadScript({
         // Loads Google Maps script into component
-        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+        libraries: ["places"]
     })
 
     if (!isLoaded) return <h2>Loading Google Maps...</h2>
@@ -23,14 +30,13 @@ export const Map = () => {
                     center={center}
                     zoom={14}
                     mapContainerStyle={{ width: '100%', height: '100%'}}
-                    options={{
-                        streetViewControl: true,
-                        mapTypeControl: true
-                    }}
+                    // options={{
+                    //     streetViewControl: true,
+                    //     mapTypeControl: true
+                    // }}
                 >
                     <Marker position={center} />
                 </GoogleMap>
-
             </div>
         </div>
     )
