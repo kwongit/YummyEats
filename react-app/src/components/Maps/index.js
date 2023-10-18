08c9ce47b1194d4e85e41fc9d18c2f4826e16143
Kenny Doan <kdoan93@gmail.com>
2023-10-18 00:29:13 -0400

    Finished implementing GoogleMaps API and added CSS to maps modal

--- react-app/src/components/Maps/index.js
+++ react-app/src/components/Maps/index.js
@@ -1,56 +1,76 @@
-import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api'
+import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api'
 import "./Maps.css"
-
-
-const center = { lat: 37.8029, lng: -122.4484 }
-
-const restaurantAddy = { lat: 37.7368185, lng: -122.4052979 }
-
-
-const getLatLonForCity = async (restaurant) => {
-    // const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
-    // console.log("apiKey: ", apiKey)
-    const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(restaurant.address + restaurant.city)}&key=AIzaSyA9q0mwj3_OD5eQngrpPd3jxRaPCcXF5ZA`
-    const geocodeResponse = await fetch(geocodeUrl)
-    const geocodeData = await geocodeResponse.json()
-    // console.log("geocodeUrl: ", geocodeUrl)
-    // console.log("geocodeResponse: ", geocodeResponse)
-    // console.log("geocodeData: ", geocodeData)
-    const { lat, lng } = geocodeData.results[0].geometry.location
-    // console.log("lat, lng: ", lat)
-    return { lat: lat, lng: lng }
-}
-
+import { useEffect, useState } from 'react';
 
 export const Map = ({ restaurant }) => {
+    const { isLoaded } = useJsApiLoader({
+        id: "google-map-script",
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+    });
 
+    const [mapCenter, setMapCenter] = useState({ lat: 37.8029, lng: -122.4484 });
 
-    const { isLoaded } = useLoadScript({
-        // Loads Google Maps script into component
-        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    })
+    console.log("Restaurant: ", restaurant)
 
-    if (!isLoaded) return <h2>Loading Google Maps...</h2>
+    const getLatLonForCity = async (restaurant) => {
+        try {
+            const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${
+                encodeURIComponent(restaurant.address + restaurant.city)}&key=AIzaSyA9q0mwj3_OD5eQngrpPd3jxRaPCcXF5ZA`;
+                console.log("LOOK: ", geocodeUrl)
+            const geocodeResponse = await fetch(geocodeUrl);
+            const geocodeData = await geocodeResponse.json();
+
+            if (geocodeData.results.length > 0) {
+                const lat = geocodeData.results[0].geometry.location.lat;
+                const lng = geocodeData.results[0].geometry.location.lng;
+
+                setMapCenter({ lat, lng });
+            }
+        } catch (error) {
+            console.error("Error fetching geocoding data:", error);
+        }
+    };
+
+    useEffect(() => {
+        if (isLoaded) {
+            getLatLonForCity(restaurant);
+        }
+    }, [isLoaded, restaurant]);
+
+    if (!isLoaded) return <h2>Loading Google Maps...</h2>;
 
     return (
         <div className='map-container'>
-            <h2>Google Map</h2>
             <div
-                style={{ width: "500px", height: "500px" }}
+                className='map-box'
+                style={{ width: "500px", height: "200px" }}
                 id="map">
                 <GoogleMap
-                    center={getLatLonForCity(restaurant)}
-                    // center={restaurantAddy}
-                    zoom={14}
-                    mapContainerStyle={{ width: '100%', height: '100%'}}
-                    // options={{
-                    //     streetViewControl: true,
-                    //     mapTypeControl: true
-                    // }}
+                    center={mapCenter}
+                    zoom={18}
+                    mapContainerStyle={{ width: '100%', height: '100%' }}
                 >
-                    <Marker position={restaurantAddy} />
+                    <Marker position={mapCenter} />
                 </GoogleMap>
             </div>
+            <div className='map-title'>
+                <h1>{restaurant.name} - {restaurant.address}</h1>
+                <h4>{restaurant.type} · {restaurant.price === 3 ? "$$$" : restaurant.price === 2 ? "$$" : "$"}</h4>
+            </div>
+            <div className='map-body'>
+                <i class="fa-solid fa-location-dot"></i>
+                <p>
+                    {restaurant.address}, {restaurant.city}, {restaurant.state}
+                </p>
+            </div>
+            <div className='map-body'>
+                <i class="fa-solid fa-clock"></i>
+                <p>Hours: {restaurant.open_hours} am - {restaurant.close_hours} pm</p>
+            </div>
+            <div className='map-body'>
+                <i className="fa-solid fa-star"></i>
+                <p>{restaurant.avg_rating} ( {restaurant.num_reviews} reviews)</p>
+            </div>
         </div>
-    )
-}
+    );
+};
